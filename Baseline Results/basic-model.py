1a4fb6a8e894dd46ccbcf4c72ddd102ef057d577
Vedant Patil <74593220+Vedant0925@users.noreply.github.com>
2023-03-06 16:42:57 +0530

    Update basic-model.py

--- Baseline Results/basic-model.py
+++ Baseline Results/basic-model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 import nltk
+import sklearn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
