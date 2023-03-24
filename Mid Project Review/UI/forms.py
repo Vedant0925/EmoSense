9fe706592cced644d4a7d8f6b7ac7d0633d19c61
Vedant Patil <74593220+Vedant0925@users.noreply.github.com>
2023-03-25 00:01:40 +0530

    Delete forms.py

--- Mid Project Review/UI/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class MoodForm(FlaskForm):
-    mood = StringField('Enter your mood:', validators=[DataRequired()])
-    submit = SubmitField('Get Recommendations')
