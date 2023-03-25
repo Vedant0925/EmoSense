167b2d436db15862d3af63cff34a841a636e3b17
Vedant Patil <74593220+Vedant0925@users.noreply.github.com>
2023-03-26 00:27:36 +0530

    Update GUI.py

--- Mid Project Review/Final Model/GUI.py
+++ Mid Project Review/Final Model/GUI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from MidReviewModel import recommend_songs, play_song, get_lyrics, sentiment_score, train_model, create_dataset, sp
+from MidModelWorking import recommend_songs, play_song, get_lyrics, sentiment_score, train_model, create_dataset, sp, evaluate_model, total_relevant
 import pickle
 import os
 
@@ -37,10 +37,16 @@ def on_recommend_click():
     recommended_songs = recommend_songs(df, model, scaler, user_mood)
     update_song_list(recommended_songs)
 
+liked_songs = set()
+disliked_songs = set()
+
 def update_song_list(songs):
     listbox_songs.delete(0, tk.END)
     for _, row in songs.iterrows():
-        listbox_songs.insert(tk.END, f"{row['title']} by {row['artist']}")
+        song_entry = f"{row['title']} by {row['artist']}"
+        if song_entry not in disliked_songs:
+            listbox_songs.insert(tk.END, song_entry)
+
 
 def on_play_click():
     selected_song = listbox_songs.get(listbox_songs.curselection())
@@ -71,5 +77,40 @@ button_recommend.grid(row=0, column=2)
 
 listbox_songs.grid(row=1, column=0, columnspan=3)
 button_play.grid(row=2, column=1)
+
+relevant_count = 0
+total_recommended = 0
+
+
+def on_submit_feedback_click():
+    global relevant_count, total_recommended
+    feedback = feedback_var.get()
+    selected_song = listbox_songs.get(listbox_songs.curselection())
+
+    if feedback == "yes":
+        relevant_count += 1
+        liked_songs.add(selected_song)
+        messagebox.showinfo("Feedback", "We're glad you like our recommendations!")
+    elif feedback == "no":
+        disliked_songs.add(selected_song)
+        messagebox.showinfo("Feedback", "Apologies. We should do better next time.")
+    else:
+        messagebox.showwarning("Warning", "Please select an option for feedback.")
+        return
+
+    total_recommended += 1
+
+feedback_var = tk.StringVar(root)
+
+radio_yes = tk.Radiobutton(root, text="I like this", variable=feedback_var, value="yes")
+radio_no = tk.Radiobutton(root, text="Not for me", variable=feedback_var, value="no")
+button_submit_feedback = tk.Button(root, text="Submit Feedback", command=on_submit_feedback_click)
+
+
+
+radio_yes.grid(row=3, column=0)
+radio_no.grid(row=3, column=1)
+button_submit_feedback.grid(row=3, column=2)
+
 
 root.mainloop()
